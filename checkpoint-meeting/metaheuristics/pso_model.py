94f6832e1a26c8c14d77b1ac1ce36c1576a7ffd8
Tadkt <thaidoankien@gmail.com>
2024-09-04 10:20:36 +0700

    4-9 work

---
+++ checkpoint-meeting/metaheuristics/pso_model.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torchvision.models
+import torch.nn.functional as F
+import os
+import numpy as np
+import copy
+import time
+import torchvision.transforms as transforms
+import torch.optim as optim
+
+class BrainTumorNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3,16,5)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.pool = nn.MaxPool2d(2,2)
+
+        self.conv2 = nn.Conv2d(16,32,5)
+        self.bn2 = nn.BatchNorm2d(32)
+        
+        self.conv3 = nn.Conv2d(32,64,3)
+        self.bn3 = nn.BatchNorm2d(64)
+        
+        self.conv4 = nn.Conv2d(64,128,3)
+        self.bn4 = nn.BatchNorm2d(128)
+
+        self.fc1 = nn.Linear(128*5*5, 512) #fully connected layer 1
+        self.dropout1 = nn.Dropout(0.5)
+
+        self.fc2 = nn.Linear(512, 256)
+        self.dropout2 = nn.Dropout(0.5)
+
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 1)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))
+
+        x = torch.flatten(x, 1)
+
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+
+        x = F.relu(self.fc2(x))
+        x = self.dropout2(x)
+
+        x = F.relu(self.fc3)
+        x = torch.sigmoid(self.fc4)
+
+        return x
+
