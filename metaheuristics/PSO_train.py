0462706aaee58ffd4a200e257df9378082dd8402
Ha Nguyen <hanh.ba12-066@st.usth.edu.vn>
2024-09-09 00:48:29 +0700

    update

--- metaheuristics/PSO_train.py
+++ metaheuristics/PSO_train.py
@@ -11,7 +11,7 @@ import torchvision.transforms as transforms
 import os
 import matplotlib.pyplot as plt
 
-from model import *
+from pso_model import *
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -22,28 +22,28 @@ transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize()
+    transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
 ])
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.ImageFolder(root='/Users/hanguyen/Documents/Prj_Brain_Tumor/Project-Brain-Tumor-MRI/data/Training', train=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
-testset = torchvision.datasets.ImageFolder(root='/Users/hanguyen/Documents/Prj_Brain_Tumor/Project-Brain-Tumor-MRI/data/Testing', train=False, transform=transform_train)
-testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=True, num_workers=2)
+trainset = torchvision.datasets.ImageFolder(root='/Users/hanguyen/Documents/Prj_Brain_Tumor/Project-Brain-Tumor-MRI/data/Training', transform=transform_train)
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+testset = torchvision.datasets.ImageFolder(root='/Users/hanguyen/Documents/Prj_Brain_Tumor/Project-Brain-Tumor-MRI/data/Testing', transform=transform_train)
+test_loader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=True, num_workers=2)
 
 classes = ('glioma','meningioma','notumor','pituitary')
 
 models = []
 for i in range(30):
-    # models.append(VGG("VGG3").to(device))
-    models.append(LeNet().to(device))
+    models.append(VGG("VGG13").to(device))
+    # models.append(LeNet().to(device))
     # models[i].apply(weights_init)
 
 criterion = nn.CrossEntropyLoss()
-result = PSO_train(models, criterion, 10, Wmax=0.95, Wmin=0.25, c1=2, c2=2, trainloader=trainloader, testloader=testloader)
+result = PSO_train(models, criterion, 10, Wmax=0.95, Wmin=0.25, c1=2, c2=2, train_loader=train_loader, test_loader=test_loader)
 
 
 # acc-epoch
