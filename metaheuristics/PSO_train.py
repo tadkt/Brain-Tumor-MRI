8e84426ede9b8a8031572534338c8f303392deb2
Tadkt <thaidoankien@gmail.com>
2024-09-11 22:01:47 +0700

    commit 11/9

--- metaheuristics/PSO_train.py
+++ metaheuristics/PSO_train.py
@@ -15,44 +15,48 @@ from pso_model import *
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# Data 
-print('Preparing data...')
+def main():
+    # Data 
+    print('Preparing data...')
+    print(device)
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
+    ])
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
+    ])
 
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
-])
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
-])
+    trainset = torchvision.datasets.ImageFolder(root=r'G:\My Drive\B3 Year Data Science (Gen 12)\Project Brain Cancer\Project-Brain-Tumor-MRI\data\Testing', transform=transform_train)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+    testset = torchvision.datasets.ImageFolder(root=r'G:\My Drive\B3 Year Data Science (Gen 12)\Project Brain Cancer\Project-Brain-Tumor-MRI\data\Training', transform=transform_train)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=True, num_workers=2)
 
-trainset = torchvision.datasets.ImageFolder(root='/Users/hanguyen/Documents/Prj_Brain_Tumor/Project-Brain-Tumor-MRI/data/Training', transform=transform_train)
-train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
-testset = torchvision.datasets.ImageFolder(root='/Users/hanguyen/Documents/Prj_Brain_Tumor/Project-Brain-Tumor-MRI/data/Testing', transform=transform_train)
-test_loader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=True, num_workers=2)
+    classes = ('glioma','meningioma','notumor','pituitary')
 
-classes = ('glioma','meningioma','notumor','pituitary')
+    models = []
+    for i in range(30):
+        models.append(VGG("VGG13").to(device))
+        # models.append(LeNet().to(device))
+        # models[i].apply(weights_init)
 
-models = []
-for i in range(30):
-    models.append(VGG("VGG13").to(device))
-    # models.append(LeNet().to(device))
-    # models[i].apply(weights_init)
-
-criterion = nn.CrossEntropyLoss()
-result = PSO_train(models, criterion, 10, Wmax=0.95, Wmin=0.25, c1=2, c2=2, train_loader=train_loader, test_loader=test_loader)
+    criterion = nn.CrossEntropyLoss()
+    result = PSO_train(models, criterion, 10, Wmax=0.95, Wmin=0.25, c1=2, c2=2, train_loader=train_loader, test_loader=test_loader)
 
 
-# acc-epoch
-plt.figure()
-plt.plot(result['train_acc_history'], label='train')
-plt.plot(result['test_acc_history'], label='test')
-plt.xlabel('epoch')
-plt.ylabel('acc')
-plt.title('acc history')
-plt.legend(loc='upper left')
-plt.savefig("checkpoint_pso/epoch_acc.png")
-plt.show()
+    # acc-epoch
+    plt.figure()
+    plt.plot(result['train_acc_history'], label='train')
+    plt.plot(result['test_acc_history'], label='test')
+    plt.xlabel('epoch')
+    plt.ylabel('acc')
+    plt.title('acc history')
+    plt.legend(loc='upper left')
+    plt.savefig("checkpoint_pso/epoch_acc.png")
+    plt.show()
+
+if __name__ == '__main__':
+    main()
